3f2fa9c1abc38bd8f86042fdcecc6f022ff8db25
Matthew Woodard <matthew.cyl.training@hotmail.com>
2022-10-23 17:02:20 -0400

    created btn and functionality to connect to metamask

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
-if (typeof window.ethereum !== 'undefined') {
-  console.log('Metamask present');
-} else {
-  console.log('Metamask not present');
+const connectSuccess = document.getElementById('connectButton');
+const tagLine = document.querySelector('.tagline');
+
+async function connect() {
+  if (typeof window.ethereum !== 'undefined') {
+    await window.ethereum.request({ method: 'eth_requestAccounts' });
+    connectSuccess.innerHTML = 'Connected';
+    tagLine.innerHTML = 'Shall we play a game?';
+  } else {
+    console.log('Metamask not present');
+  }
 }

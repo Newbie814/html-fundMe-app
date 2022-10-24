c62b9af11c776d9aefce9455c6e147d5009e329a
Matthew Woodard <matthew.cyl.training@hotmail.com>
2022-10-23 22:34:07 -0400

    added try catches to connect function

--- app.js
+++ app.js
@@ -1,12 +1,29 @@
 const connectSuccess = document.getElementById('connectButton');
 const tagLine = document.querySelector('.tagline');
 
+// async function connect() {
+//   if (typeof window.ethereum !== 'undefined') {
+//     await window.ethereum.request({ method: 'eth_requestAccounts' });
+//     connectSuccess.innerHTML = 'Connected';
+//     tagLine.innerHTML = 'Shall we play a game?';
+//   } else {
+//     connectSuccess.innerHTML = 'Not Found!!';
+//     tagLine.innerHTML = 'Please install MetaMask';
+//   }
+// }
+
 async function connect() {
   if (typeof window.ethereum !== 'undefined') {
-    await window.ethereum.request({ method: 'eth_requestAccounts' });
-    connectSuccess.innerHTML = 'Connected';
+    try {
+      await ethereum.request({ method: 'eth_requestAccounts' });
+    } catch (error) {
+      console.log(error);
+    }
+    connectButton.innerHTML = 'Connected';
     tagLine.innerHTML = 'Shall we play a game?';
+    const accounts = await ethereum.request({ method: 'eth_accounts' });
+    console.log(accounts);
   } else {
-    console.log('Metamask not present');
+    connectButton.innerHTML = 'Please install MetaMask';
   }
 }
